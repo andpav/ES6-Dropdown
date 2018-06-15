9becdd1c62885fc5ebb255bdb18a090f074ccb3d
Andrey Pavlenko <andpav@Andreys-MacBook-Pro.local>
2018-06-16 02:07:15 +0300

    dropdown logic, list item, multiselect

--- src/Dropdown/index.js
+++ src/Dropdown/index.js
@@ -1,60 +1,60 @@
 import '../css/styles.css';
 
+export class ListItem {
+  constructor(id, name, photo, showPhoto, multiSelect, isSelected, selectItem) {
+    this.node = document.createElement('div');
+    this.node.className = 'list-item';
+
+    this.node.onclick = () => {
+      multiSelect ? this.node.classList.toggle('list-item_selected') : selectItem(id);
+    }
+
+    if (isSelected) {
+      this.node.classList.add('list-item_selected')
+    }
+
+    if (showPhoto) {
+      const photoElement = document.createElement('div');
+      this.node.appendChild(photoElement);
+    }
+
+    const nameElement = document.createElement('span');
+    nameElement.textContent = name;
+
+    this.node.appendChild(nameElement);
+
+    this.render = this.render.bind(this);
+  }
+
+  render() {
+    return this.node;
+  }
+}
+
 export default class Dropdown {
-  constructor(items) {
+  constructor(items, autoComplete, multiSelect, useServer, showPhoto) {
     this.items = items;
+    this.autoComplete = autoComplete;
+    this.multiSelect = multiSelect;
+    this.showPhoto = showPhoto;
+    this.selectedItem = null;
 
     this.node = document.createElement('div');
     this.node.className = 'dropdown';
 
     this.input = document.createElement('input');
 
-    this.list = document.createElement('div');
-    this.list.className = 'dropdown__list';
-
-    // this.initEventHandlers();
-    this.add = this.add.bind(this);
-    this.hide = this.hide.bind(this);
-    this.toggle = this.toggle.bind(this);
     this.render = this.render.bind(this);
     this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    this.selectItem = this.selectItem.bind(this);
 
     document.addEventListener('click', this.handleOutsideClick, false);
   }
-  // show(item) {
-  //     item.classList.add(this.activeClass);
-  // }
-  // hide(item) {
-  //     item.classList.remove(this.activeClass);
-  // }
-  // initEventHandlers() {
-  //     const _this = this;
-  //
-  //     for (let i = 0, len = this.items.length; i < len; i += 1) {
-  //         const item = this.items[i];
-  //
-  //         item.addEventListener('click', function(e) {
-  //             e.preventDefault();
-  //             if (this.classList.contains(_this.activeClass)) {
-  //                 _this.hide(this);
-  //             } else {
-  //                 _this.show(this);
-  //             }
-  //         });
-  //     }
-  // }
- handleOutsideClick(className) {
-    this.classList.add(className);
-  }
-  add(className) {
-    this.classList.add(className);
-  }
-  hide(className) {
-    this.classList.remove(className);
-  }
 
-  toggle(className) {
-    this.classList.toggle(className);
+  selectItem(id) {
+    this.selectedItem = id;
+
+    this.render();
   }
 
   handleOutsideClick(e) {
@@ -64,12 +64,32 @@ export default class Dropdown {
   }
 
   render() {
+    if (this.list) {
+      this.node.removeChild(this.list);
+    }
+
     this.input.type = 'text';
     this.input.className = 'dropdown__input';
     this.input.onclick = () => this.list.classList.add('dropdown__list_shown');
 
-    this.node.appendChild(this.input)
-    this.node.appendChild(this.list)
+    /*
+    * Решение прокидывать знание о мультиселектности в сами айтемы у меня вызывает боль.
+    * Однако хранить в дропдауне при мультиселекте список всех выбранных айтемов, удалять их тоже так себе идея,
+    * хотя архитектурно более правильная. Может зафигачить какую-нибудь клевую структуру данных?
+     */
+
+    this.list = document.createElement('div');
+    this.list.className = 'dropdown__list';
+    this.items.map(item => {
+      const wrapper = document.createElement('div');
+
+      wrapper.appendChild((new ListItem(item.id, item.name, item.photo, this.showPhoto, this.multiSelect, item.id === this.selectedItem, this.selectItem)).render());
+
+      this.list.appendChild(wrapper);
+    });
+
+    this.node.appendChild(this.input);
+    this.node.appendChild(this.list);
 
     return this.node;
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,30 @@ import Dropdown from './Dropdown/index';
 
 let container = document.createElement('div');
 
+const items = [
+  {
+    id : '1',
+    name : 'Vasily Slonikhin',
+    photo : '...',
+  },
+  {
+    id : '2',
+    name : 'Ivan Molotov',
+    photo : '...',
+  },
+  {
+    id : '3',
+    name : 'Boris the Blade',
+    photo : '...',
+  },
+];
+
+const autoComplete = false;
+const multiSelect = false;
+const useServer = false;
+const showPhoto = false;
+
 container.className = 'container';
-container.appendChild((new Dropdown).render());
+container.appendChild((new Dropdown(items, autoComplete, multiSelect, useServer, showPhoto)).render());
 
 document.body.appendChild(container);
